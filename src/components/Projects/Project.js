b05c81a57e41b57483d67f6b259ec7fc3a38dc5d
nicholas <nicholaspheld@gmail.com>
2021-07-23 08:52:47 -0500

    working on first project js display and syntax

---
+++ src/components/Projects/Project.js
@@ -0,0 +1,34 @@
+import React from 'react';
+import './Projects.css';
+import court from './images/court.png';
+
+const projectData = [
+
+    {
+        title: 'Court Finder',
+        repoName: 'court-finder',
+        deployedName: 'https://quiet-beyond-45478.herokuapp.com/',
+        image: court,
+        description: 'This is a web application to network with others looking to join or schedule pick-up basketball games '
+    }
+];
+
+export function Projects() {
+    return projectData.map((project, index) => {
+        return (
+            <div className='card' key={index}>
+                <h2>{project.title}</h2>
+                <div className='cardContent'> 
+                    <div>
+                        <img src={project.image} alt={project.title} />
+                        <p>{project.description}</p>
+                    </div>
+                    <div>
+                        <p><a href={project.deployedName}>Deployed App</a></p>
+                        <p><a href={`https://github.com/nicholasheld/${project.repoName}`}>GitHub</a></p>
+                    </div>
+                </div>
+            </div>
+        )
+    })
+}
